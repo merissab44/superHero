1e342f82c6edfd8e5406adab6f3c6a1bf879a4bc
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2020-11-13 14:17:00 -0800

    finished ability and shield classes

---
+++ ability.py
@@ -0,0 +1,16 @@
+import random
+class Ability:
+    def __init__(self, name, max_damage):
+        # initialized values that will be passed when 
+        # we call the class
+        self.name = name
+        self.max_damage = max_damage
+
+    def attack(self):
+        random_value = random.randint(0, self.max_damage)
+        return random_value
+
+if __name__ == "__main__":
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
